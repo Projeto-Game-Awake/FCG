e620d8e153e4ebee663b6cc4fd8b5b468970b8d4
Bruno Duarte <bruno.duartec@gmail.com>
2021-05-25 23:45:56 -0300

    Add side to card

--- js/classes/Deck.js
+++ js/classes/Deck.js
@@ -1,18 +1,18 @@
-var CardSide = ["lights","shadows"];
+var CardSide = ["lights", "shadows"];
 class Deck {
-    constructor(x,y,side=0,type=0){
-        this.sprite = scene.add.sprite(x,y,"decks",type);
-        this.side=side;
-        this.cards = [
-            new Card(0),
-            new Card(0),
-            new Card(0),
-            new Card(1),
-            new Card(1),
-            new Card(1),
-            new Card(2),
-            new Card(2),
-            new Card(2),
-        ]
-    }
+  constructor(x, y, side = 0, type = 0) {
+    this.sprite = scene.add.sprite(x, y, "decks", type);
+    this.side = side;
+    this.cards = [
+      new Card(0, side),
+      new Card(0, side),
+      new Card(0, side),
+      new Card(1, side),
+      new Card(1, side),
+      new Card(1, side),
+      new Card(2, side),
+      new Card(2, side),
+      new Card(2, side),
+    ];
+  }
 }
